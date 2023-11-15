3c4172c7af881b4520761178772410c1ba43d0e8
Francsy <francsyludens@gmail.com>
2023-11-16 00:32:10 +0100

    add/app layout for all views

--- resources/views/add-note.blade.php
+++ resources/views/add-note.blade.php
@@ -1,21 +1,25 @@
-        @include('layouts.header')
-        <main class="content">
-            <div class="cards">
-                <div class="card card-center">
-                    <div class="card-body">
-                        <h1>Nueva nota</h1>
-
-                        <form action="">
-                            <label for="title" class="field-label">Título: </label>
-                            <input type="text" name="title" id="title" class="field-input">
-
-                            <label for="content" class="field-label">Contenido:</label>
-                            <textarea name="content" id="content" rows="10" class="field-textarea"></textarea>
-
-                            <button type="submit" class="btn btn-primary">Crear nota</button>
-                        </form>
-                    </div>
-                </div>
+@extends('layouts.app')
+@section('title', 'Añadir una nota')
+    
+@section('content')
+    
+<main class="content">
+    <div class="cards">
+        <div class="card card-center">
+            <div class="card-body">
+                <h1>Nueva nota</h1>
+                
+                <form action="">
+                    <label for="title" class="field-label">Título: </label>
+                    <input type="text" name="title" id="title" class="field-input">
+                    
+                    <label for="content" class="field-label">Contenido:</label>
+                    <textarea name="content" id="content" rows="10" class="field-textarea"></textarea>
+                    
+                    <button type="submit" class="btn btn-primary">Crear nota</button>
+                </form>
             </div>
-        </main>
-        @include('layouts.footer')
+        </div>
+    </div>
+</main>
+@endsection
