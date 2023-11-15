d09dc94784aafaa681f59c75624db1927fb21680
Francsy <francsyludens@gmail.com>
2023-11-15 13:58:39 +0100

    add/example routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/notas', function () {
+    return 'Listado de notas';
+});
+
+Route::get('/notas/crear', function () {
+    return 'Crear nueva nota';
+});
+
+// Dificultad Random:
+Route::get('/cursos', function () {
+    return [
+        'cursos'=> [
+            ['curso' => 'PHP', 'dificultad' => rand(1, 10)],
+            ['curso' => 'Laravel', 'dificultad' => rand(1, 10)],
+            ['curso' => 'JS', 'dificultad' => rand(1, 10)]
+        ]
+    ];
+});
